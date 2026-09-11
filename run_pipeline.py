3e2410ae6478afc342380818f2dad814e2b02ab8
GitHub Action <action@github.com>
2026-09-11 04:23:14 +0000

    CHORE: Update data integrity hash

--- run_pipeline.py
+++ run_pipeline.py
@@ -87,7 +87,7 @@ from scipy.stats import zscore
 # 1. CONFIGURATION
 # =======================================================
 raw_dataset_url = "https://raw.githubusercontent.com/Ritwik240/Weather-Dataset/refs/heads/main/Unified_Weather_Dataset_Latest.json"
-OFFICIAL_DATASET_HASH = "96501f062b051148387f2f79e08d824d5dd2a0e9b00abf6881bdddaf8588ceac"
+OFFICIAL_DATASET_HASH = "c667b50b175a9dfd7eb8582dc61bee187b0dcee9cc28819e49068a5a2de4e65b"
 
 # =======================================================
 # 2. HASH CHECK FUNCTION
